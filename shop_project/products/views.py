000613ac18ab33f5f12aa00eccd2040c9f0ec435
Eva <evacherkashyna9@gmail.com>
2025-12-29 19:16:35 +0200

    Update views.py

--- shop_project/products/views.py
+++ shop_project/products/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.core.paginator import Paginator
 
-# Create your views here.
+from .models import Product
+
+
+def product_list(request):
+    all_products = Product.objects.all()
+    paginator = Paginator(all_products, 4)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'products/list.html', {'page_obj': page_obj})
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'products/detail.html', {'product': product})
